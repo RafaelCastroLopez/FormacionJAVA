723e3d5ff1d3d0217ebfa45e4a6c93bd0edb8f4f
Rafael Castro Lopez <rafael.castro@bosonit.com>
2022-09-19 10:29:22 +0200

    modificarAEjercicio11.1CORS

--- Ejercicio7/Ejercicio7.1/CRUDConValidacion/src/main/java/com/example/CRUDConValidacion/persona/infraestructure/controller/outPut/PersonCreateController.java
+++ Ejercicio7/Ejercicio7.1/CRUDConValidacion/src/main/java/com/example/CRUDConValidacion/persona/infraestructure/controller/outPut/PersonCreateController.java
@@ -3,6 +3,7 @@ package com.example.CRUDConValidacion.persona.infraestructure.controller.outPut;
 import com.example.CRUDConValidacion.persona.application.PersonaService;
 import com.example.CRUDConValidacion.persona.infraestructure.controller.inPut.PersonaInputDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,8 @@ public class PersonCreateController {
     @Autowired
     PersonaService personaService;
 
-    @PostMapping("persona")
+    @PostMapping("/addperson")
+    @CrossOrigin
     public PersonaOutPutDto addPersona(@RequestBody PersonaInputDto personaInputDto) throws Exception{
         return personaService.addPersona(personaInputDto);
     }
--- Ejercicio7/Ejercicio7.1/CRUDConValidacion/src/main/java/com/example/CRUDConValidacion/persona/infraestructure/controller/outPut/PersonFindController.java
+++ Ejercicio7/Ejercicio7.1/CRUDConValidacion/src/main/java/com/example/CRUDConValidacion/persona/infraestructure/controller/outPut/PersonFindController.java
@@ -2,6 +2,7 @@ package com.example.CRUDConValidacion.persona.infraestructure.controller.outPut;
 
 import com.example.CRUDConValidacion.persona.application.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,8 @@ public class PersonFindController {
         return personaService.getPersonaByName(name);
     }
 
-    @GetMapping("persona/todos")
+    @GetMapping("/getall")
+    @CrossOrigin
     public List<PersonaOutPutDto> getAll(){
         return personaService.getAllPerson();
     }
